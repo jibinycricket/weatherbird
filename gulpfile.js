4c5b3ce7f30bfbebf57bb15b406950bc41f54eeb
jmathew1991 <jmathew1991@gmail.com>
2016-06-08 21:29:29 -0400

    Added Gulp Task to Concat and Minify JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 //Styles Task: Convert sass files to css
 gulp.task('styles', function() {
@@ -13,8 +16,21 @@ gulp.task('styles', function() {
         .pipe(livereload());
 });
 
-//Watch task
+//Combine and minify Javascript Files
+gulp.task('scripts', function(){
+  return gulp.src('./public/javascripts/*.js')
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest('./public/javascripts'))
+    .pipe(rename('application.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/javascripts'));
+});
+
+//Watch CSS
 gulp.task('default', function(){
   livereload.listen();
   gulp.watch('public/stylesheets/sass/*.sass', ['styles']);
 });
+
+
+
